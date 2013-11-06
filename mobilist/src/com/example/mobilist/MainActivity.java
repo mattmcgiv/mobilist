fd4e6bbc9efafa3fa753aac4030d2c4a4240707e
mcgivney <matthew.mcgivney@gmail.com>
2013-11-06 04:56:34 -0500

    Added second activity and linked it to "Add" button.

--- mobilist/src/com/example/mobilist/MainActivity.java
+++ mobilist/src/com/example/mobilist/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.mobilist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
 
 public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.droidforhumans.mobilist.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when user touches "Add" button */
+	public void addItem(View view) {
+		// Creates Intent to call the ListActivity
+		Intent intent = new Intent(this, ListActivity.class);
+		EditText editText = (EditText) findViewById(R.id.message_add);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
